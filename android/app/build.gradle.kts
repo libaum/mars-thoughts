cd1d8e3f043add0144c3eb2e9061cdd09c14e3d9
Linus Baumgaertner <lbaumgaertner@culba.de>
2026-09-26 23:32:28 +0200

    chore: AGP 9.2.1, Kotlin 2.3.20, Gradle 9.4.1

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -2,7 +2,6 @@ import java.util.Properties
 
 plugins {
     id("com.android.application")
-    id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
@@ -19,10 +18,6 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-    }
-
-    kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
     }
 
     defaultConfig {
@@ -80,6 +75,12 @@ androidComponents {
             if (variant.buildType == "debug") append(" Debug")
         }
         variant.manifestPlaceholders.put("appLabel", label)
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
     }
 }
 
